45680579f751b0e3cf65df8feadd2a0a6d28a605
Vincent Ivos <vincentivos1@gmail.com>
2020-09-16 01:42:07 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -27,7 +27,7 @@ def output_array(array)
  end
 end 
 
-=begin
+
 def return_string_array(array)
   count = 0
   new_array =  []
@@ -35,22 +35,10 @@ def return_string_array(array)
   while count < array.length
 
   new_array.push(array[count].to_s)
-  count =+ 1
+  count += 1
   end
 
   new_array
 end
-=end 
-
-def return_string_array(array)
-  count = 0
-
-  while count < array.length
-
-  array[count] = array[count].to_s
-  count =+ 1
-  end
-
-  array
-end
  
+
