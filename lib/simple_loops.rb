7afe843f40d4b9e985417ef322a4542e39298f25
Vincent Ivos <vincentivos1@gmail.com>
2020-09-16 01:18:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,11 @@
 
 def loop_message_five_times(message)
   count = 0 
-  
+  while count < 5 
+  do
+    puts message 
+    count += 1 
+  end 
 end 
 =begin 
 def loop_message_n_times(message, limit)
