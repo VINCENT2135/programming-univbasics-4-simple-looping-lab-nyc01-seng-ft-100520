faed7c9adb0fcbe5dba1fbed5e3e41bc45ae2268
Vincent Ivos <vincentivos1@gmail.com>
2020-09-16 01:14:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,5 +1,5 @@
 # Write your methods here
-=begin
+
 def loop_message_five_times(message)
   count = 0 
   while count < 5 
@@ -8,7 +8,7 @@ def loop_message_five_times(message)
     counts += 1 
   end 
 end 
-
+=begin 
 def loop_message_n_times(message, limit)
   count = 0
   while count < limit 
