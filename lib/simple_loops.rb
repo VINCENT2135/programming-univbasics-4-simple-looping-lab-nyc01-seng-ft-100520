8a00d0bae8a47f4b9f7f7b708457476e46d96eac
Vincent Ivos <vincentivos1@gmail.com>
2020-09-16 01:20:48 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -12,7 +12,7 @@ end
 def loop_message_n_times(message, limit)
   count = 0
   while count < limit 
-  do
+  
     puts message
     count += 1 
   end
@@ -22,7 +22,6 @@ end
 def output_array(array)
    count = 0
    while count < array.length 
- do
   puts array[count]
   count += 1 
  end
@@ -32,10 +31,10 @@ def return_string_array(array)
   count = 0
   new_array =  []
   while count < array.length
-  do
+
   new_array.push(array[count].to_s)
   count =+ 1
   end
   new_array
 end
-=end 
+ 
